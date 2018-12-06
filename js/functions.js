4bd28e6ef32e998761508a0cb2c188353a9bd79a
alfonsof <alfonsofb@gmail.com>
2018-12-06 15:38:42 +0100

    Added activateTab function

--- js/functions.js
+++ js/functions.js
@@ -14,3 +14,8 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+// active a tab in a Bootstrap JS Tab
+function activeTab(tabID){
+    $('.nav-tabs a[href="#' + tabID + '"]').tab('show');
+};
